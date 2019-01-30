bfcd1700bdb845f7c41393996b8195f90ad629d4
RohanSWilkho93 <46768608+RohanSWilkho93@users.noreply.github.com>
2019-01-30 17:19:18 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,75 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.length == 0
+  sum = 0
+  arr.each do |num|
+    sum += num
+  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.length == 0
+  return arr[0] if arr.length == 1
+  max1 = arr.max
+  y = arr.find_index(max1)
+  arr.delete_at(y)
+  max2 = arr.max
+  return max1 + max2
 end
 
+
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  for i in (0...arr.length())
+    for j in (0...arr.length())
+      next if i == j
+      return true if arr[i].to_i+arr[j].to_i == n
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "<< name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return true if s!='' && !(s[0] =~ /([aeiou])/i) && !(s[0] =~ /\W/ )
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return true if s!='' && !(s =~ /((\D+)([2-9]*))/) && (s.to_i)%4 == 0
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+attr_accessor :isbn, :price
+  def initialize isbn, price
+    @isbn = isbn
+    @price = price.to_f
+    raise ArgumentError, "Invalid ISBN - Empty String", caller if @isbn.length == 0
+    raise ArgumentError, "Invalid Price", caller if @price<=0.0
+  end
+  def price_as_string
+    p = (@price.to_s).chars
+    x = p.find_index('.')
+    y = p[x...p.length]
+    z = 3-y.length
+    if y.length<3
+      z.times do p.push('0') end
+    end
+    if y.length>3
+      g=true if (y[3].to_i)>=5
+      a = -z
+      a.times do p.pop end
+      p[-1] = (p[-1].to_i + 1).to_s if g==true
+    end
+    return "$#{p.join}"
+  end
 end
